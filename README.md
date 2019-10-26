d8610ad0dee4d17c8df0cfe88398a8e160204eda
Gihovani Filipp Pereira Demétrio <gihovani@gmail.com>
2019-10-26 13:02:12 -0300

    feito processo para facilitar a instalação da aplicação

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# encomenergia
+# Site da Encom Energia
+
+Aplicação precisa dos serviços:
+- mariadb ou mysql 
+- php
+- apache
+- composer
+
+Passos para instalar a aplicação:
+
+- Instalar Dependências do projeto
+```
+composer install
+```
+
+- Editar arquivo:
+    - application/config/database.php
+
+- Rodar as migrações para incluir os dados no banco de dados:    
+```
+php index.php migrate index
+```
+
+# Autores 
+[Gihovani Filipp](https://gg2.com.br)
+[Guilherme Sousa](https://guilhermesousa.com.br) 
+
+
+
--- application/controllers/Migrate.php
+++ application/controllers/Migrate.php
@@ -4,6 +4,8 @@
  *
  * @copyright Copyright © 2019 GG2 Solucoes. All rights reserved.
  * @author    gihovani@gmail.com
+ * @property CI_Input $input
+ * @property CI_Output $output
  */
 
 class Migrate extends CI_Controller
@@ -12,14 +14,28 @@ class Migrate extends CI_Controller
      * @var CI_Migration
      */
     public $migration;
+	private function show($text)
+	{
+		$this->output
+			->set_output($text)
+			->_display();
+		exit();
+	}
     public function index()
     {
+    	$message = 'Não foi possivel fazer a migração.';
+    	if (!is_cli()) {
+			show_error($message);
+		}
         $this->load->library('migration');
-        if ($this->migration->current() === FALSE)
-        {
+        if ($this->migration->current() === FALSE) {
             show_error($this->migration->error_string());
         }
-        print $this->migration->latest();
-        exit;
+
+		$latestMigrate = $this->migration->latest();
+        if (!$latestMigrate) {
+			show_error($message);
+		}
+		return $this->show('Migração feita com sucesso: ' . $latestMigrate);
     }
 }
