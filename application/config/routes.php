e0af62882b9c850fce4fcdd06f61aff40f76d6ee
Gihovani Filipp Pereira Demétrio <gihovani@gmail.com>
2019-10-31 12:04:30 -0300

    criado o arquivo robots.txt

--- application/config/routes.php
+++ application/config/routes.php
@@ -50,6 +50,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |		my-controller/my-method	-> my_controller/my_method
 */
 $route['default_controller'] = 'pages/view';
+$route['sitemap\.xml'] = 'sitemap/index';
 $route['files/(:any)'] = 'pages/files/$1';
 $route['(:any)'] = 'pages/view/$1';
 
